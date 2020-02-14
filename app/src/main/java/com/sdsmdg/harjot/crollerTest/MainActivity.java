95c64ea26379ebdd9ab6cf71897ff03d6516c0b9
Araz Farhang Dareshuri <a.farhang.d@gmail.com>
2020-02-15 03:15:57 +0800

    Switched to Android X

--- app/src/main/java/com/sdsmdg/harjot/crollerTest/MainActivity.java
+++ app/src/main/java/com/sdsmdg/harjot/crollerTest/MainActivity.java
@@ -1,38 +1,46 @@
 package com.sdsmdg.harjot.crollerTest;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SwitchCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private Croller croller;
+    private Croller croller1;
+
+    private LinearLayout wrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Croller croller = new Croller(this);
+        croller1 = findViewById(R.id.croller);
+        wrapper = findViewById(R.id.wrapper);
 
-        croller = findViewById(R.id.croller);
         SwitchCompat enableSwitch = findViewById(R.id.enableSwitch);
 
         enableSwitch.setChecked(croller.isEnabled());
 
-//        croller.setIndicatorWidth(10);
-//        croller.setBackCircleColor(Color.parseColor("#EDEDED"));
-//        croller.setMainCircleColor(Color.WHITE);
-//        croller.setMax(50);
-//        croller.setStartOffset(45);
-//        croller.setIsContinuous(false);
-//        croller.setLabelColor(Color.BLACK);
-//        croller.setProgressPrimaryColor(Color.parseColor("#0B3C49"));
-//        croller.setIndicatorColor(Color.parseColor("#0B3C49"));
-//        croller.setProgressSecondaryColor(Color.parseColor("#EEEEEE"));
-//        croller.setProgressRadius(380);
-//        croller.setBackCircleRadius(300);
+        croller.setIndicatorWidth(10);
+        croller.setBackCircleColor(Color.parseColor("#EDEDED"));
+        croller.setMainCircleColor(Color.WHITE);
+        croller.setMax(100);
+        croller.setStartOffset(50);
+        croller.setIsContinuous(false);
+        croller.setLabelColor(Color.BLACK);
+        croller.setProgressPrimaryColor(Color.parseColor("#0B3C49"));
+        croller.setIndicatorColor(Color.parseColor("#0B3C49"));
+        croller.setProgressSecondaryColor(Color.parseColor("#EEEEEE"));
+        croller.setProgressRadius(380);
+        croller.setBackCircleRadius(300);
 
+        wrapper.addView(croller);
 
         croller.setOnCrollerChangeListener(new OnCrollerChangeListener() {
             @Override
@@ -55,12 +63,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (b) {
-                    croller.setEnabled(true);
+                    croller1.setEnabled(true);
                 } else {
-                    croller.setEnabled(false);
+                    croller1.setEnabled(false);
                 }
             }
         });
 
     }
 }
+
